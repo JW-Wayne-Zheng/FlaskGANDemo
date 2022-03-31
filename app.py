06e6528f4197433417d557f0f6fabe442d59ba8f
Wayne-Z-Code-To-Great <60633000+Wayne-Z-Code-To-Great@users.noreply.github.com>
2022-03-31 17:19:09 -0400

    Little update

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 from flask import Flask, render_template
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+from bs4 import BeautifulSoup
+
+
+soup = BeautifulSoup("templates/index.html")
+
+# model = load_model("models/monet_generator.h5")
+
+# print(model.summary())
+
 
 app = Flask(__name__)
 
