15a57917fcbe8997a7b8b6c295a0802cb68f9e81
Wayne Zheng <wnz6klj@gmail.com>
2022-04-12 22:25:50 -0400

    Recreate the generator and attempt to predict

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
-from tensorflow import keras
+from tensorflow.keras.models import load_model
 import tensorflow_addons as tfa
 from bs4 import BeautifulSoup
-
+import cv2
+import tensorflow as tf
+import PIL
+import numpy as np
+import generator
 
 soup = BeautifulSoup("templates/index.html")
-
-model = keras.models.load_model("models/monet_generator.h5", custom_objects={"InstanceNormalization":tfa.layers.InstanceNormalization})
-
+model = generator.generator_fn()
+model.load_weights("models/monet_generator.h5")
+# model = load_model("models/monet_generator.h5", custom_objects={"InstanceNormalization":tfa.layers.InstanceNormalization})
 print(model.summary())
 
+img = cv2.imread("test-images\Webp.net-resizeimage.jpg")
+img = tf.reshape(img, [1, 256, 256, 3])
+prediction = model(img, training=False)[0].numpy() # make predition
+prediction = (prediction * 127.5 + 127.5).astype(np.uint8)   # re-scale
+im = PIL.Image.fromarray(prediction)
+im.save("output.jpg")
 
 app = Flask(__name__)
 
